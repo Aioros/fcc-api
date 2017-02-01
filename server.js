71944403bf95747486455a597ab412975d5175e9
Stefano Morciano <aioros83@gmail.com>
2017-02-01 11:16:59 +0000

    Main logic and mini homepage

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', express.static(__dirname));
+
+app.get('/:input', function (req, res) {
+  
+  var input = req.params["input"];
+  var date = null;
+  var result = { unix: null, natural: null };
+  
+  if (input.match(/^\d{10}$/)) {
+    date = new Date(parseInt(input) * 1000);
+  } else if (input.match(/^\w{3,} \d{1,2}, \d{2,4}$/)) {
+    date = new Date(input);
+  }
+  
+  if (date) {
+    result.unix = Math.floor(date.getTime() / 1000);
+    result.natural = date.toLocaleString("en-en", { month: "long", day: "2-digit", year: "numeric" });
+  }
+  
+  res.send(result);
+  
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('App listening on port 8080');
 })
